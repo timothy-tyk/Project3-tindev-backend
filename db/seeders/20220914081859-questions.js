a805834f8511aff7802d8d51ac0d8aaa0c2c0050
timothy-tyk <timothytan94@gmail.com>
2022-09-26 11:16:27 +0800

    backend debugging

--- db/seeders/20220914081859-questions.js
+++ db/seeders/20220914081859-questions.js
@@ -8,9 +8,9 @@ module.exports = {
         title: "How to open VSCode?",
         details: `{"blocks":[{"key":"as43o","text":"test","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}`,
         tokens_offered: 5,
-        mentor_id: null,
+        mentor_id: 2,
         lobby_id: 1,
-        solved: false,
+        solved: true,
         img_url:
           "https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fimageio.forbes.com%2Fspecials-images%2Fdam%2Fimageserve%2F958761228%2F960x0.jpg%3Fformat%3Djpg%26width%3D960",
         created_at: new Date(),
@@ -21,9 +21,9 @@ module.exports = {
         title: "Javascript HELP!!",
         details: `{"blocks":[{"key":"as43o","text":"test","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}`,
         tokens_offered: 25,
-        mentor_id: null,
+        mentor_id: 1,
         lobby_id: 1,
-        solved: false,
+        solved: true,
         img_url:
           "https://i.picsum.photos/id/169/200/300.jpg?hmac=5EPn5kvK71xgHn-1BAILeGz45FNBGasUBZQulf9HOmU",
         created_at: new Date(),
